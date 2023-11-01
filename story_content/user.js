64581665157cac8f0acddd43c9de8088e269da09
HodaBarghash <121548764+HodaBarghash@users.noreply.github.com>
2023-11-01 11:38:26 +0200

    g

--- story_content/user.js
+++ story_content/user.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5WiS8OeIuEk":
+      case "6XEFLopvHl9":
         Script1();
         break;
   }
@@ -20,7 +20,7 @@ fetch(url,{
 	cache:'no-cache',
 	headers: {'Content-Type': 'application/json'},
 	redirect:'follow',
-	body: JSON.stringify({v10q1attempts:player.GetVar("v20q1attempts"),v20q1answeredcorrect:player.GetVar("v20q1answeredcorrect"),v20q2attempts:player.GetVar("v20q2attempts"),v20q2answeredcorrect:player.GetVar("v20q2answeredcorrect"),v20q3attempts:player.GetVar("v20q3attempts"),v20q3answeredcorrect:player.GetVar("v20q3answeredcorrect")})
+	body: JSON.stringify({v20q1attempts:player.GetVar("v20q1attempts"),v20q1answeredcorrect:player.GetVar("v20q1answeredcorrect"),v20q2attempts:player.GetVar("v20q2attempts"),v20q2answeredcorrect:player.GetVar("v20q2answeredcorrect"),v20q3attempts:player.GetVar("v20q3attempts"),v20q3answeredcorrect:player.GetVar("v20q3answeredcorrect")})
 	}
 	)
 }
